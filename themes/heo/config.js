00cd0e490c34c044e1d3b9f19d092931c31061ca
懒人 <145119825+Lazy-zed@users.noreply.github.com>
2024-07-03 16:45:02 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-07-02', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到lazy-zed博客', url: 'https://lazy-zed.com' },
+    { title: '努力成为AI和渲染大佬的第n天', url: 'https://lazy-zed.com' }，
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -16,16 +16,16 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_2: '与游戏开发',
+  HEO_HERO_TITLE_3: 'lazy-zed.com',
+  HEO_HERO_TITLE_4: '最新发布',
+  HEO_HERO_TITLE_5: '总有光照亮前路',
+  HEO_HERO_TITLE_LINK: 'https://lazy-zed.com',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '游戏理论', url: '/tag/游戏理论' },
+  HEO_HERO_CATEGORY_2: { title: '游戏实践', url: '/tag/游戏实践' },
+  HEO_HERO_CATEGORY_3: { title: '杂文随记', url: '/tag/杂文随记' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -34,21 +34,19 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
+    '游戏开发客户端',
+    '🔍 喜欢渲染与AI',
+    '🤝 专心参加gamejam',
     '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/Lazy-zed',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://www.lazy-zed.com',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
