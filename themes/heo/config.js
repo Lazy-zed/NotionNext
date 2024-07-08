5f7f5ec14f20b0cba44fe961d60224e21872649b
懒人 <145119825+Lazy-zed@users.noreply.github.com>
2024-07-08 15:15:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到lazy-zed博客', url: 'https://lazy-zed.com' },
-    { title: '努力成为AI和渲染大佬的第n天', url: 'https://lazy-zed.com' },
+    { title: '欢迎来到lzzd的博客', url: 'https://lazy-zed.com' },
+    { title: '努力学好渲染的第n天', url: 'https://lazy-zed.com' },
   ],
 
   // 英雄区左右侧组件颠倒位置
