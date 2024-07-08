d3f09360e969a9635dc0f259eca9d2c87740f554
懒人 <145119825+Lazy-zed@users.noreply.github.com>
2024-07-08 15:13:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,10 +24,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'lazy-zed', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'lzzd', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '想一千个idea不如做一个demo', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://lazy-zed.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Lazed博客,LazedBlog,记录生活,分享知识,工具,游戏开发,U3D,AI,渲染', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Lzzd博客,LzzdBlog,记录生活,分享知识,工具,游戏开发,U3D,AI,渲染', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'hjh1715332905@163.com', // 邮箱地址 
